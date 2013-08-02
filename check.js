d1e05027fcf561810d064bf8b06be192880cc09a
gmadytinos <gmadytinos@gmail.com>
2013-08-02 18:10:57 +0200

    polished javascript and options page

--- check.js
+++ check.js
@@ -2,16 +2,20 @@ setInterval(function() {
   chrome.storage.local.get(null, function(items) {	
 	var username = items['username'];
 	var password = items['password'];
+        var url = "https://bulksms.2way.co.za/eapi/user/get_credits/1/1.1?username="+username+"&password="+password;
+
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "http://bulksms.2way.co.za:5567/eapi/user/get_credits/1/1.1?username="+username+"&password="+password, true);
+	xhr.open("GET", url, true);
 	xhr.onreadystatechange = function() {
 	  if (xhr.readyState == 4) {
+	    var credits = xhr.responseText.substr(xhr.responseText.indexOf('|')+1, 
+		Math.min(xhr.responseText.length-(xhr.responseText.indexOf('.')+1), 4));
+            if (credits.length > 25) credits = "?";
 	    chrome.browserAction.setBadgeText({
-		text: xhr.responseText.substr(xhr.responseText.indexOf('|')+1, 
-		      Math.min(xhr.responseText.length-(xhr.responseText.indexOf('|')+2), 4)) 
-	    });
-	  }
-	}
+	  	text: credits
+      	    });
+          }
+        }
 	xhr.send();
   });
 }
--- options.js
+++ options.js
@@ -1,27 +1,40 @@
 // Saves options to localStorage.
 function save_options() {
-  var username_ctl = document.getElementById("username");
-  var username = username_ctl.value;
+  var username = $("#username").val();
+  var password = $("#password").val();
+  var url = "https://bulksms.2way.co.za/eapi/user/get_credits/1/1.1?username="+username+"&password="+password;
 
-  var password_ctl = document.getElementById("password");
-  var password = password_ctl.value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var credits = xhr.responseText.substr(xhr.responseText.indexOf('|')+1, 
+		Math.min(xhr.responseText.length-(xhr.responseText.indexOf('.')+1), 4));
+      if (credits.length > 25) credits = "?";
+      chrome.browserAction.setBadgeText({
+  	text: credits
+      });
+    }
+  }
+  xhr.send();
 
   chrome.storage.local.set({'username':username, 'password':password}, function() {
-	  // Update status to let user know options were saved.
-	  var status = document.getElementById("status");
-	  status.style.display = "block";
+	  $("#status").css("display","block").fadeOut(2000);
   });
-
 }
 
 // Restores state to saved value from localStorage.
 function restore_options() {
   chrome.storage.local.get(null, function(items) {	
-	var username_ctl = document.getElementById("username");
-	var password_ctl = document.getElementById("password");
-	username.value = items['username'];
-	password.value = items['password'];
+	if (items['username'] && items['password']) {
+	  $("#username").val(items['username']);
+	  $("#password").val(items['password']);
+	}
   });
+  $("#status").css("display","none");
 }
+
+$(document).foundation();
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
+
